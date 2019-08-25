dd118458914b0b91b5ba81a9d1254ce218e7e0fa
cletsxdv <jclementehdzhdz@gmail.com>
2019-08-25 13:23:18 -0500

    Some comments

--- Python trials/exp2_jchh.py
+++ Python trials/exp2_jchh.py
@@ -3,8 +3,11 @@
 import fileinput
 import sys, glob
 
+# Toma cualquier nombre del libro con comienzo "Busi_" y terminación ".txt"
 archivos = glob.glob("../Management books/Busi_*.txt")
 archivos.sort()
+
+# Toma cada nombre de archivo
 for linea in fileinput.input(archivos, openhook=fileinput.hook_encoded("utf-8")):
     if fileinput.isfirstline():
     	# Files name
