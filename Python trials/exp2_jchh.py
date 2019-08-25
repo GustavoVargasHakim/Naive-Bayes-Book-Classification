66d57c742c0fe09cd8cf61e69b5544535a7495e4
cletsxdv <jclementehdzhdz@gmail.com>
2019-08-25 13:21:14 -0500

    Actualización ej2 para tomar nombres de archivos

--- Python trials/exp2_jchh.py
+++ Python trials/exp2_jchh.py
@@ -6,6 +6,7 @@ import sys, glob
 archivos = glob.glob("../Management books/Busi_*.txt")
 archivos.sort()
 for linea in fileinput.input(archivos, openhook=fileinput.hook_encoded("utf-8")):
-    if not fileinput.isfirstline():
-        linea = linea.rstrip()
-        print(linea)
+    if fileinput.isfirstline():
+    	# Files name
+        ar = fileinput.filename()
+        print(ar)
