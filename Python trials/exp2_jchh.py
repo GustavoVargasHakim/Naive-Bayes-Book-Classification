8e195c41c70660ac33d7b8e4f843ffae79bbc64e
cletsxdv <jclementehdzhdz@gmail.com>
2019-08-25 13:44:05 -0500

    ej2 lee archivos y une nltk para la carpeta de Social Science

--- Python trials/exp2_jchh.py
+++ Python trials/exp2_jchh.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+import nltk
+import string
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
 import fileinput
 import sys, glob
 
@@ -10,6 +14,30 @@ archivos.sort()
 # Toma cada nombre de archivo
 for linea in fileinput.input(archivos, openhook=fileinput.hook_encoded("utf-8")):
     if fileinput.isfirstline():
-    	# Files name
-        ar = fileinput.filename()
-        print(ar)
+        # Files name
+        book = fileinput.filename()
+
+        book_1 = open(book, encoding="utf-8").read()
+
+        book1 = nltk.word_tokenize(book_1)
+
+        stop_words = set(stopwords.words('english')) 
+
+        filtered_book = [w for w in book1 if not w in stop_words]
+
+        single_character = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'eg',
+                      'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'σi', 'σn',
+                      'α', 'β', 'βn', 'xn', 'αv', 'ν', 'ϕ', 'ba', 'ip', 'fi', 'kr', 'fr', 'ij', 
+                      'bd', 'nj', 'ac', 'bd', 'hk', 'gc', 'xg', 'dn', 'bi', 'mn', 'αu', 'hg', 
+                      'zn', 'nth', 'mmc','gcd', 'cd', 'ub', 'di', 'ad', 'ab','gh', 'στ', 'σ', 'ai',
+                      'cis', 'abab', 'aabb', 'id', 'sn', 'ax', 'bx', 'αn','px', 'acr', 'bcs', 'hn',
+                      'kx', 'ζ', 'η', 'θ', 'κ', 'λ', 'μ', 'ξ', 'ρ', 'τ', 'φ', 'χ', 'ψ', 
+                      'ω', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+                      'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Ω', 'Ψ', 'Σ', 'Π', 
+                      'Ξ', 'Λ', 'Θ', 'Δ', 'Γ', 'aβ', 'aβj', 'βj', 'gf', 'pn', 'bp', 'zp', 
+                      'bch')
+
+        filtered_book = [w for w in filtered_book if not w in single_character]
+
+        print(" -------- LIBRO -------- ")
+        print(filtered_book)
