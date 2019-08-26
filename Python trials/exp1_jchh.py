5f674690adcde306f95962296bc26bb8c4f6eb3d
cletsxdv <jclementehdzhdz@gmail.com>
2019-08-25 19:19:22 -0500

    Commit para eliminar algunos archivos

--- Python trials/exp1_jchh.py
+++ Python trials/exp1_jchh.py
@@ -7,7 +7,7 @@ import sys, glob
 import time
 
 # Toma cualquier nombre del libro con comienzo "Busi_" y terminación ".txt"
-archivos = glob.glob("../Management books/Busi_*.txt")
+archivos = glob.glob("../Social Sciences books/Soci_*.txt")
 archivos.sort()
 
 for linea in fileinput.input(archivos, openhook=fileinput.hook_encoded("utf-8")):
@@ -21,20 +21,20 @@ for linea in fileinput.input(archivos, openhook=fileinput.hook_encoded("utf-8"))
 
         Busi1=[w.lower() for w in Busi1 if w.isalpha()]
 
-        stop_words = set(stopwords.words('english')) 
+        stop_words = set(stopwords.words('english'))
 
         filtered_book = [w for w in Busi1 if not w in stop_words]
 
         single_character = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'eg',
                               'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'σi', 'σn',
-                              'α', 'β', 'βn', 'xn', 'αv', 'ν', 'ϕ', 'ba', 'ip', 'fi', 'kr', 'fr', 'ij', 
-                              'bd', 'nj', 'ac', 'bd', 'hk', 'gc', 'xg', 'dn', 'bi', 'mn', 'αu', 'hg', 
+                              'α', 'β', 'βn', 'xn', 'αv', 'ν', 'ϕ', 'ba', 'ip', 'fi', 'kr', 'fr', 'ij',
+                              'bd', 'nj', 'ac', 'bd', 'hk', 'gc', 'xg', 'dn', 'bi', 'mn', 'αu', 'hg',
                               'zn', 'nth', 'mmc','gcd', 'cd', 'ub', 'di', 'ad', 'ab','gh', 'στ', 'σ', 'ai',
                               'cis', 'abab', 'aabb', 'id', 'sn', 'ax', 'bx', 'αn','px', 'acr', 'bcs', 'hn',
-                              'kx', 'ζ', 'η', 'θ', 'κ', 'λ', 'μ', 'ξ', 'ρ', 'τ', 'φ', 'χ', 'ψ', 
+                              'kx', 'ζ', 'η', 'θ', 'κ', 'λ', 'μ', 'ξ', 'ρ', 'τ', 'φ', 'χ', 'ψ',
                               'ω', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
-                              'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Ω', 'Ψ', 'Σ', 'Π', 
-                              'Ξ', 'Λ', 'Θ', 'Δ', 'Γ', 'aβ', 'aβj', 'βj', 'gf', 'pn', 'bp', 'zp', 
+                              'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Ω', 'Ψ', 'Σ', 'Π',
+                              'Ξ', 'Λ', 'Θ', 'Δ', 'Γ', 'aβ', 'aβj', 'βj', 'gf', 'pn', 'bp', 'zp',
                               'bch')
 
         filtered_book = [w for w in filtered_book if not w in single_character]
